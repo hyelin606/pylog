cba0bfbd7e3b74e0ad24a4cac5094cf724dbfc9c
HyelinChoi <imhyell@naver.com>
2024-01-24 23:06:04 +0900

    UPLOADED new project file2

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from blog.models import Post, Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+# Register your models here.
